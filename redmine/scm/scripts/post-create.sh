67492c638a23ae5bc734d9c4e40d4ee5fd652e69
ogis-ito <Ito_Yoshiichi@ogis-ri.co.jp>
2021-03-24 18:06:00 +0900

    Apache+SVN 追加 (権限制御なし)。

---
+++ redmine/scm/scripts/post-create.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCM_REPO_PATH=$1
+SCM_TYPE=$2
+SCM_PROJECT=$3
+
+SCM_REPO_URL=file://$SCM_REPO_PATH
+
+case "$SCM_TYPE" in
+    svn)
+        svn mkdir $SCM_REPO_URL/{trunk,branches,tags} \
+            --username admin -m "Add trunk, branches, tags."
+        ;;
+    git)
+        ;;
+    *)
+        echo "SCM not supported: $SCM_TYPE" >&2
+        ;;
+esac
+
+exit 0
---
+++ scm/run_apache.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eux
+
+mkdir -p /home/scm/repos/git
+mkdir -p /home/scm/repos/svn
+#chown -R www-data /home/scm/repos
+
+/etc/init.d/apache2 start
+
+tail -F /var/log/apache2/*.log
